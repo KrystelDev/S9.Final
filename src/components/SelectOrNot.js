f045821dcb624859b48b932188604dfcdcb43450
krystel <krystel.dev@gmail.com>
2022-02-21 12:32:54 +0100

    open and close

--- src/components/SelectOrNot.js
+++ src/components/SelectOrNot.js
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 const SelectOrNot = ({ viewDisplayPokemon }) => {
   return (
     <div className="pokeindex-right__buttons-double">
       <button type="button" onClick={viewDisplayPokemon}>
         Select
       </button>
-      <button>List</button>
+      <Link to="/">
+        <button>Exit</button>
+      </Link>
     </div>
   );
 };
